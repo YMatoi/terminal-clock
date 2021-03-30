2da798e7d94292ab641f4cab0ad7f5f2650ebdb5
YMatoi <YMatoi@users.noreply.github.com>
2021-03-30 23:32:11 +0900

    use async_stdin for exit

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,36 @@
+extern crate regex;
 extern crate termion;
 
 use chrono::Local;
+use figlet_rs::FIGfont;
+use regex::Regex;
+use std::io::{stdout, Read, Write};
 use std::thread;
 use std::time::Duration;
-use figlet_rs::FIGfont;
+use termion::async_stdin;
+use termion::raw::IntoRawMode;
 
 fn main() {
     let font = FIGfont::standand().unwrap();
+    let re = Regex::new("\n").unwrap();
+    let stdout = stdout();
+    let mut stdout = stdout.lock().into_raw_mode().unwrap();
+    let mut stdin = async_stdin().bytes();
+
     loop {
-        print!("{}{}", termion::clear::All, termion::cursor::Goto(1,1));
+        let b = stdin.next();
+        if let Some(Ok(b'q')) = b {
+            break;
+        }
+        write!(stdout, "{}", termion::clear::All);
+        write!(stdout, "{}q: exit", termion::cursor::Goto(1, 1));
+
         thread::sleep(Duration::from_millis(1000));
         let time = format!("{}", Local::now().format("%r"));
         let figure = format!("{}", font.convert(&time).unwrap());
-        print!("{}", figure);
+        let figure = re.replace_all(&figure, "\n\r");
+        write!(stdout, "{}{}", termion::cursor::Goto(1, 2), figure);
+
+        stdout.flush().unwrap();
     }
 }
